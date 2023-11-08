688be2d8178d28366868e345723b948b41b030db
Harishmalika <gharishece74@gmail.com>
2023-11-08 22:05:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+
+def jampandu(request):
+    return HttpResponse('hi hoe are you')
+
+
+def harish(request):
+    return HttpResponse('hell0 harish')
